1d372b65ac31a7b9790ed51770ca74c9bf3050ee
Andrey <kouznts@ya.ru>
2020-01-16 22:47:43 +0400

    1.3

--- 1.3.rkt
+++ 1.3.rkt
@@ -6,6 +6,18 @@
   (* a a))
 
 (define
-  (return-sum-of-squares-of-two-num a b)
+  (return-sum-of-squares-of-two-nums a b)
   (+ (return-square-of-num a)
      (return-square-of-num b)))
+
+(define
+  (return-sum-of-squares-of-two-larger-nums a b c)
+  (if (> a b)
+      (if (> b c)
+          (return-sum-of-squares-of-two-nums a b)
+          (return-sum-of-squares-of-two-nums a c))
+      (if (> a c)
+          (return-sum-of-squares-of-two-nums a b)
+          (return-sum-of-squares-of-two-nums b c))))
+
+(return-sum-of-squares-of-two-larger-nums 1 2 3)
