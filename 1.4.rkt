21eb2c03fc98d2c78eb84436001e3145d4feb912
Andrey <kouznts@ya.ru>
2020-01-17 09:01:58 +0400

    #lang racket

--- 1.4.rkt
+++ 1.4.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |1.4|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang racket
+
 (define
   (a-plus-abs-b a b)
   ((if (> b 0)
